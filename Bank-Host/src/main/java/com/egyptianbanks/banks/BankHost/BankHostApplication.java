837f1e6e359323c94a3d632dd7a81c4dfbd7737d
Beshoy Gerges Software Development Engineer <b.gerges@egyptianbanks.net>
2019-08-18 11:08:57 +0200

    print besho 3

--- Bank-Host/src/main/java/com/egyptianbanks/banks/BankHost/BankHostApplication.java
+++ Bank-Host/src/main/java/com/egyptianbanks/banks/BankHost/BankHostApplication.java
@@ -8,7 +8,7 @@ public class BankHostApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankHostApplication.class, args);
-		System.out.println("besho 2");
+		System.out.println("besho 3");
 	}
 
 }

0e1c0ebed52b683a338925845a8958e9b54d464b
Beshoy Gerges Software Development Engineer <b.gerges@egyptianbanks.net>
2019-08-18 10:47:29 +0200

    create the project structure

---
+++ Bank-Host/src/main/java/com/egyptianbanks/banks/BankHost/BankHostApplication.java
@@ -0,0 +1,13 @@
+package com.egyptianbanks.banks.BankHost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankHostApplication.class, args);
+	}
+
+}
